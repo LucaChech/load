e1f7055b0626e16717f25c862b6c9d09e0c56a81
nilli lab <nilli lab@nilli17.psychol.ucl.ac.uk>
2017-10-10 18:26:02 +0100

    script now processes all participants and creates htm tables

--- analysis/process.py
+++ analysis/process.py
@@ -79,12 +79,16 @@ def process(df):
         if df.loc[i,'trial_type'] == 'Critical' and (not pd.isnull(df.loc[i,'RT_TO'])) and df.loc[i,'tone_onset'] != -999:
             df.loc[i,'RT_TO'] = df.loc[i,'RT_TO'] - df.loc[i,'tone_onset']
     
-    #Correctness
-    
+    #Correctness for Visual-search task
+    #Accuracy for the visual-search task: we want trials:
+    #- with a target (either present or absent) : 'trial_type' == 'Target Present' or 'trial_type' == 'Target Absent'
+    #- without false alarms to the tone: pd.isnull(df.loc[i,'RT_TO'])
+    #- without the tone : df.loc[i,'trial_type'] == 'Normal'
+    #tone-detection task
     df['correct_vs'] = None
-    
+     
     for i in range_from_1(si):
-        if df.loc[i,'trial_type'] == 'Normal':
+        if df.loc[i,'trial_type'] == 'Normal' and pd.isnull(df.loc[i,'RT_TO']):
             if df.loc[i,'present_absent'] == 'Target Present'  and df.loc[i,'keys'] == 'q':
                 df.loc[i,'correct_vs'] = 1
             if df.loc[i,'present_absent'] == 'Target Present' and df.loc[i,'keys'] == 'p':
@@ -97,6 +101,8 @@ def process(df):
     n_corrects_vs = len(df[df['correct_vs'] == 1])
     print 'CORRECT VS: ',n_corrects_vs
     
+    
+    ### This is for calculating Accuracy (properly hit rate) for the detection task
     df['restricted_correct_tone'] = None
     
     for i in range_from_1(si):
@@ -152,18 +158,49 @@ def process(df):
     n_present_low = len(df[df['sdt'].notnull() & (df['trial_type'] == 'Critical') & (df['load'] == 'low')])
     n_hit_low = len(df[df['sdt'] == 1 & (df['load'] == 'low')])
     hit_rate_low = float(n_hit_low) / n_present_low
+    tone_detection_diff = hit_rate_low - hit_rate_high
     
-   
-            
+    
+    #Accuracy for the visual-search task: we want trials:
+    #- with a target (either present or absent) : 'trial_type' == 'Target Present' or 'trial_type' == 'Target Absent'
+    #- without false alarms to the tone
+    #- without the tone
+    #tone-detection task
+    
+    #High load
+    n_vs_trials_with_target_high = len(df[(df['present_absent'] != 'No Target') & (df['trial_type'] == 'Normal') & (pd.isnull(df['RT_TO'])) & (df['load']=='high')])
+    n_vs_trials_correct_high = len(df[(df['load']=='high') & (df['correct_vs']==1)])
+    accuracy_vs_high = float(n_vs_trials_correct_high) / float(n_vs_trials_with_target_high)
+    #
+    #Low load
+    n_vs_trials_with_target_low = len(df[(df['present_absent'] != 'No Target') & (df['trial_type'] == 'Normal') & (pd.isnull(df['RT_TO'])) & (df['load']=='low')])
+    n_vs_trials_correct_low = len(df[(df['load']=='low') & (df['correct_vs']==1)])
+    accuracy_vs_low = float(n_vs_trials_correct_low) / float(n_vs_trials_with_target_low)
+    
+    accuracy_vs_diff = accuracy_vs_low - accuracy_vs_high
+    
+    ### Calculating absolute number of false alarms for the detection task
+    
+    #High load
+    fa_high = len(df[(df['trial_type']=='Normal') & (df['RT_TO'].notnull()) & (df['load']=='high')])
+    fa_low = len(df[(df['trial_type']=='Normal') & (df['RT_TO'].notnull()) & (df['load']=='low')])
+    
+    
+    
+    
     for i in range_from_1(si):
         if df.loc[i,'valid'] == 0:
             df.loc[i,'sdt'] = 0    
             
     #Result storage
-    
-    notes['tone_accuracy'] = df['correct_tone'].mean()
-    notes['tone_hit_rate_high'] = hit_rate_high
-    notes['tone_hit_rate_low'] = hit_rate_low
+    notes['vs_accuracy_high'] = accuracy_vs_high
+    notes['vs_accuracy_low'] = accuracy_vs_low
+    notes['vs_accuracy_diff'] = accuracy_vs_diff
+    notes['tone_detection_accuracy_high'] = hit_rate_high
+    notes['tone_detection_accuracy_low'] = hit_rate_low
+    notes['tone_detection_accuracy_diff'] = tone_detection_diff
+    notes['false_alarms_high'] = int(fa_high)
+    notes['false_alarms_low'] = int(fa_low)
 
     #print notes
     return [df, notes]
